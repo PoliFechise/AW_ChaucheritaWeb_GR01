0804f33d3df23fc2c49c0b06fe81d710c2023d47
Sebasky26 <sebasaisalla26@gmail.com>
2025-02-03 01:34:53 -0500

    Correccion en nombres jspql CU 7 Listar Categorias

--- src/main/java/model/dao/CategoriaDAO.java
+++ src/main/java/model/dao/CategoriaDAO.java
@@ -26,21 +26,21 @@ public class CategoriaDAO {
 
 	// Método para listar las categorías de egreso
 	public List<Categoria> obtenerCategoriasEgreso() {
-		String jpql = "SELECT c FROM CatEgreso c";
+		String jpql = "SELECT c FROM CatEgreso catEgreso";
 		Query query = em.createQuery(jpql);
 		return query.getResultList();
 	}
 
 	// Método para listar las categorías de ingreso
 	public List<Categoria> obtenerCategoriasIngreso() {
-		String jpql = "SELECT c FROM CatIngreso c";
+		String jpql = "SELECT c FROM CatIngreso catIngreso";
 		Query query = em.createQuery(jpql);
 		return query.getResultList();
 	}
 
 	// Método para listar las categorías de transferencia
 	public List<Categoria> obtenerCategoriasTransferencia() {
-		String jpql = "SELECT c FROM CatTransferencia c";
+		String jpql = "SELECT c FROM CatTransferencia catTransferencia";
 		Query query = em.createQuery(jpql);
 		return query.getResultList();
 	}
